77c1c921cb46b2aef842d3be6ae6b9df341c36e9
Henrique Zuchetto Rossi <hzrossi@outlook.com>
2016-06-10 13:02:02 -0300

    primeiro commit

--- Busca_ISBN/ViewController.swift
+++ Busca_ISBN/ViewController.swift
@@ -11,8 +11,10 @@ import UIKit
 class ViewController: UIViewController {
 
     @IBOutlet weak var txtISBN: UITextField!
-    @IBOutlet weak var txtViewResultado: UITextView!
-    
+	@IBOutlet weak var txtViewResultado: UITextView!
+
+	//isbn teste: 0201075253 / 0201531747 / 978-84-376-0494-7
+
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -40,9 +42,11 @@ class ViewController: UIViewController {
     }
     
     func buscaSincrona(isbn:String) {
-        let urls = "https://openlibrary.org/api/books?jscmd=data&format=json&bibkeys=ISBN:" + isbn
+		let isbnBusca = "ISBN:" + isbn
+        let urls = "https://openlibrary.org/api/books?jscmd=data&format=json&bibkeys=" + isbnBusca
         let url = NSURL(string: urls)
         let dados:NSData? = NSData(contentsOfURL: url!)
+
 		// se resultado nulo, está sem internet
         if (dados == nil) {
             //cria o alerta
@@ -51,22 +55,73 @@ class ViewController: UIViewController {
             
             //mostra o alerta
             self.presentViewController(alerta, animated: true, completion: nil)
+
+			self.txtViewResultado.text = ""
         }
         else {
-            let texto = NSString(data: dados!, encoding: NSUTF8StringEncoding)
-			//se resultado for vazio ou chaves, não encontrou o ISBN
-            if (texto! == "" || texto! == "{}") {
-                //cria o alerta
-                let alerta = UIAlertController(title: "Aviso!", message: "ISBN não encontrado", preferredStyle: UIAlertControllerStyle.Alert)
-                alerta.addAction(UIAlertAction(title: "OK", style: UIAlertActionStyle.Cancel, handler: nil))
-                
-                //mostra o alerta
-                self.presentViewController(alerta, animated: true, completion: nil)
-            }
-            else {
-                txtViewResultado.text = texto! as String
-            }
-        }
+			do {
+				let json = try NSJSONSerialization.JSONObjectWithData(dados!, options: NSJSONReadingOptions.MutableLeaves)
+
+				let resultado = json as! NSDictionary //armazena os dados do livro
+
+				//se livro não contém elementos (está vazio), não encontrou o livro
+				if (resultado.count == 0) {
+					//cria o alerta
+					let alerta = UIAlertController(title: "Aviso!", message: "ISBN não encontrado", preferredStyle: UIAlertControllerStyle.Alert)
+					alerta.addAction(UIAlertAction(title: "OK", style: UIAlertActionStyle.Cancel, handler: nil))
+
+					//mostra o alerta
+					self.presentViewController(alerta, animated: true, completion: nil)
+					self.txtViewResultado.text = ""
+				}
+				
+				//senão encontrou um livro
+				else {
+					print("encontrei")
+
+					let livro = resultado["\(isbnBusca)"] as! NSDictionary
+					let titulo = livro["title"] as! NSString as String
+					print(titulo)
+					self.txtViewResultado.text = "Título: " + titulo + "\n"
+
+					//isso - autor
+					let autores = livro["authors"] as! NSArray
+					self.txtViewResultado.text = txtViewResultado.text + "Autor(es):\n"
+					for i in 0 ..< autores.count {
+						let nome = autores[i]["name"] as! NSString as String
+						print(nome)
+						self.txtViewResultado.text = txtViewResultado.text + "\t" + nome + "\n"
+					}
+
+					/* //OU isso
+						let autores = livro["authors"] as! NSArray
+						self.txtViewResultado.text = txtViewResultado.text + "Autor(es):\n"
+						for autor in autores {
+							let nome = autor["name"] as! NSString as String
+							print(nome)
+							self.txtViewResultado.text = txtViewResultado.text + "\t" + nome + "\n"
+						}
+					*/
+
+					//isso - capa
+					if livro["cover"]?.count > 0 {
+						let capas = livro["cover"] as! NSDictionary
+						self.txtViewResultado.text = txtViewResultado.text + "Capa(s):\n"
+						let capaPequena = capas["small"] as! NSString as String
+						let capaMedia = capas["medium"] as! NSString as String
+						let capaGrande = capas["large"] as! NSString as String
+						print(capaPequena + " " + capaMedia + " " + capaGrande)
+						self.txtViewResultado.text = txtViewResultado.text + "\tPequena: " + capaPequena + "\n\tMédia: " + capaMedia + "\n\tGrande: " + capaGrande + "\n"
+					}
+					else {
+						self.txtViewResultado.text = txtViewResultado.text + "Capa(s): Indisponível"
+					}
+				}
+			}
+			catch _ {
+				print("não foi possível converter o resultado")
+			}
+		}
     }
 
 }
